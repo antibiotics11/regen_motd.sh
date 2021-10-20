72c68d5add56dda140d480ebd7cea2c3e1fa36dd
Antibiotics <abx@abx.pe.kr>
2021-10-20 10:52:44 +0900

    Changed repo name && fixed some error
    2021-10-20

--- regen_motd.sh
+++ regen_motd.sh
@@ -1,34 +1,50 @@
 #!/bin/bash
 
-# name of directory the script will operate
+# * motd_dir:
+# directory where the motd file is located
+# the default is /etc/ircd-hybrid
+
 motd_dir="/etc/ircd-hybrid";
 
-# server info to be displayed
-server_name="ANTIBIOTICS' IRC";
-server_admin="Antibiotics";
-admin_email="abx@abx.pe.kr";
+# * server_name: your irc server name 
+server_name=" ";
 
-# rename old motd file to date + .old format
+# * server_admin: server admin username
+server_admin=" ";
+
+# * admin_email: server admin email address
+admin_email="";
+
+date=$(date '+%Y-%m-%d');
 for file_list in "$motd_dir"/*; do
         file_name=$(basename -- "$file_list");
         file_ext="${file_name##*.}";
         file_name="${file_name%.*}";
         if [ $file_ext == "motd" ]; then
+                motd_not_found=false;
                 file_path=$file_list;
                 motd_file_name=$file_name;
-                date=$(date '+%Y-%m-%d');
                 mv "$file_list" "${file_list}.old.${date}";
+                break;
         fi;
+        motd_not_found=true;
 done;
 
-# select daily random ascii art
+if [ "$motd_not_found" = true ]; then
+        file_path=$motd_dir"/ircd.motd";
+fi;
+        
+
+
 arts[0]="
  __      _____________.____   _________  ________      _____  ___________
 /  \    /  \_   _____/|    |  \_   ___ \ \_____  \    /     \ \_   _____/
 \   \/\/   /|    __)_ |    |  /    \  \/  /   |   \  /  \ /  \ |    __)_ 
  \        / |        \|    |__\     \____/    |    \/    Y    \|        \
+ 
   \__/\  / /_______  /|_______ \______  /\_______  /\____|__  /_______  /
        \/          \/         \/      \/         \/         \/        \/ 
+
 ";
 arts[1]="
 Y88b         / 888~~  888      e88~-_    ,88~-_        e    e      888~~  
@@ -37,6 +53,7 @@ Y88b         / 888~~  888      e88~-_    ,88~-_        e    e      888~~
    Y88bd8b/    888    888     8888     88888    |   / Y88Y Y888b   888    
     Y88Y8Y     888    888     Y888   /  Y888   /   /   YY   Y888b  888    
      Y  Y      888___ 888____  88_-~     88_-~   /          Y888b 888___  
+
 ";
 arts[2]="
  __          ________ _      _____ ____  __  __ ______ 
@@ -47,20 +64,27 @@ arts[2]="
      \/  \/   |______|______\_____\____/|_|  |_|______|
 
 ";
+arts[3]="
+[..        [..[........[..          [..       [....     [..       [..[........
+[..        [..[..      [..       [..   [..  [..    [..  [. [..   [...[..      
+[..   [.   [..[..      [..      [..       [..        [..[.. [.. [ [..[..      
+[..  [..   [..[......  [..      [..       [..        [..[..  [..  [..[......  
+[.. [. [.. [..[..      [..      [..       [..        [..[..   [.  [..[..      
+[. [.    [....[..      [..       [..   [..  [..     [.. [..       [..[..      
+[..        [..[........[........   [....      [....     [..       [..[........
 
-arts_size=${#arts[@]};
-selected_art=$(($RANDOM % $arts_size));
+";
 
-# select daily random comments
 comment[0]="Absence makes the heart grow fonder";
 comment[1]="Actions speak louder than words";
 comment[2]="A journey of a thousand miles begins with a single step";
 comment[3]="A picture is worth a thousand words";
 comment[4]="All good things must come to an end";
+
+arts_size=${#arts[@]};
+selected_art=$(($RANDOM % $arts_size));
 comments_size=${#comment[@]};
 selected_comment=$(($RANDOM % $comments_size));
-
-# puts contents to file
 
 file_contents="
 ${arts[$selected_art]}
@@ -68,14 +92,11 @@ ________________________________________________________________________________
 
 Welcome to ${server_name}!
 
-Server Administrator: ${server_admin}
-CONTACT: ${admin_email}
+Server Administrator - ${server_admin}
+CONTACT - ${admin_email}
 
-Today's comment '${comment[$selected_comment]}'
+Thought of the Day '${comment[$selected_comment]}'
 ";
 
 echo "$file_contents" > "$file_path";
-
-# restart ircd-hyrid server
-
 /etc/init.d/ircd-hybrid restart;
